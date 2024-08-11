359a32cbe4281367cb344d02a339d7ee02fd2302
ael-kass <aelkassimi321@gmail.com>
2024-08-11 11:01:09 +0100

    handle events

--- word-masters.js
+++ word-masters.js
@@ -1,6 +1,49 @@
+const WORDL_URL = "https://words.dev-apis.com/validate-word";
+
+function isLetter(letter)
+{
+    return /^[a-zA-Z]$/.test(letter);
+}
+
+function getBoxField(caseNum)
+{
+    const boxLetter = ".box-" + caseNum;
+    const letterBox = document.querySelector(boxLetter);
+    return (letterBox);
+}
+
 function init()
 {
+    let index = 0;
+    // let lines = 0;
+    let boxObject;
 
+    document.addEventListener("keydown", 
+        function(event)
+        {
+            console.log(event.key)
+            if (event.key == "Enter" && index == 5)
+            {
+                isValidWord();
+            }
+            else if (event.key == "Backspace" && index != 0)
+            {
+                boxObject.value = "";
+                index -= 1;
+                boxObject = getBoxField(index);
+            }
+            else if (!isLetter(event.key))
+            {
+                event.preventDefault();
+            }
+            else if (isLetter(event.key) && index != 5)
+            {
+                index += 1;
+                boxObject = getBoxField(index);
+                boxObject.value = event.key.toUpperCase();
+            }
+        }
+    )
 }
 
 init();
